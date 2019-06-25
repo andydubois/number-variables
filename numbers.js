3fb2986bf61aa3bfd2fbf48a30633891e259a81a
Andy DuBois <andydubois89@gmail.com>
2019-06-24 21:56:39 -0500

    exercise practicing with number variables

---
+++ numbers.js
@@ -0,0 +1,14 @@
+console.log('js')
+
+//Declare and initialize the variables
+let testVar = 9;
+
+console.log( testVar );
+
+console.log('the value of testVar is', testVar );
+
+testVar = 7654456;
+
+console.log('the value of testVar is', testVar );
+
+testVar = 'some text';
